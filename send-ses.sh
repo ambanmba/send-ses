a9c7ce43a6bfad8eb41afc3457ea3c4558b60838
ambanmba <bor@ambor.com>
2021-08-16 14:07:52 +1000

    Ability to attach a file and auto-detect mime type

--- send-ses.sh
+++ send-ses.sh
@@ -9,12 +9,8 @@ sesToAddress="mary.mcmary@notmyrealmail.com"
 sesSubject="Test Email from Script"
 sesSMTP="email-smtp.us-east-1.amazonaws.com"
 sesPort="465"
-sesMessage="Test of line 1\nTest of line 2"
+sesMessage=$'Test of line 1\nTest of line 2'
+sesFile="$1"
+sesMIMEType=`file --mime-type $sesFile | sed 's/.*: //'`
 
-CompleteMessage="From: \"$sesFromName\" <$sesFromAddress>\nTo: \"$sesToName\" <$sesToAddress>\nSubject: $sesSubject\n\n$sesMessage"
-
-curl -v --url smtps://$sesSMTP:$sesPort --ssl-reqd  --mail-from $sesFromAddress --mail-rcpt $sesToAddress  --user $sesAccess:$sesSecret -T <(echo -e $CompleteMessage);
-
-
-
-
+curl -v --url smtps://$sesSMTP:$sesPort --ssl-reqd  --mail-from $sesFromAddress --mail-rcpt $sesToAddress  --user $sesAccess:$sesSecret -F '=(;type=multipart/mixed' -F "=$sesMessage;type=text/plain" -F "=@$sesFile;type=$sesMIMEType;encoder=base64" -F '=)' -H "Subject: $sesSubject" -H "From: $sesFromName <$sesFromAddress>" -H "To: $sesToName <$sesToAddress>"
