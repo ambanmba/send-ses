665a4e1ed6131ef118769b79b778a7ba02a7432e
ambanmba <bor@ambor.com>
2021-08-16 15:37:23 +1000

    Fix issue where attachment filename has spaces

--- send-ses.sh
+++ send-ses.sh
@@ -11,6 +11,8 @@ sesSMTP="email-smtp.us-east-1.amazonaws.com"
 sesPort="465"
 sesMessage=$'Test of line 1\nTest of line 2'
 sesFile="$1"
-sesMIMEType=`file --mime-type $sesFile | sed 's/.*: //'`
+sesMIMEType=`file --mime-type "$sesFile" | sed 's/.*: //'`
 
-curl -v --url smtps://$sesSMTP:$sesPort --ssl-reqd  --mail-from $sesFromAddress --mail-rcpt $sesToAddress  --user $sesAccess:$sesSecret -F '=(;type=multipart/mixed' -F "=$sesMessage;type=text/plain" -F "=@$sesFile;type=$sesMIMEType;encoder=base64" -F '=)' -H "Subject: $sesSubject" -H "From: $sesFromName <$sesFromAddress>" -H "To: $sesToName <$sesToAddress>"
+echo $sesMIMEType
+
+curl -v --url smtps://$sesSMTP:$sesPort --ssl-reqd  --mail-from $sesFromAddress --mail-rcpt $sesToAddress  --user $sesAccess:$sesSecret -F '=(;type=multipart/mixed' -F "=$sesMessage;type=text/plain" -F "file=@$sesFile;type=$sesMIMEType;encoder=base64" -F '=)' -H "Subject: $sesSubject" -H "From: $sesFromName <$sesFromAddress>" -H "To: $sesToName <$sesToAddress>"
